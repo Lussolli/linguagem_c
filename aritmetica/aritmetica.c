2d446d051c106214f593a04d3b9e7be5c3586b97
Lussolli <leonardo.lussolli@gmail.com>
2020-05-31 17:13:38 -0300

    Aprendendo operadores aritméticos

---
+++ aritmetica/aritmetica.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int primeiroNumero = 50,
+        segundoNumero = 25;
+    
+    int soma = primeiroNumero + segundoNumero;
+    int subtracao = primeiroNumero - segundoNumero;
+    int multiplicacao = primeiroNumero * segundoNumero;
+    int divisao = primeiroNumero / segundoNumero;
+    int resto = primeiroNumero % segundoNumero;
+    
+    printf("Resultado da soma: %i\n", soma, "\n");
+    printf("Resultado da subtracao: %i\n", subtracao);
+    printf("Resultado da multiplicacao: %i\n", multiplicacao);
+    printf("Resultado da divisao: %i\n", divisao);
+    printf("Resultado do resto da divisao: %i\n", resto);
+	    
+    return (0);
+}
+
